36616b4ed871cd292c407548c38144585121f0a6
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-02-28 14:11:31 +0200

    push

---
+++ project/main.py
@@ -0,0 +1,97 @@
+
+from fastapi import FastAPI, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+app = FastAPI()
+order_list = []  # Remove when all routse use database
+
+
+class Order(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    item: str
+    status: str = "Pending"
+    price: float
+
+
+engine = create_engine("sqlite:///./project/mcd-order.db")
+SQLModel.metadata.create_all(engine)
+
+
+@app.get("/")
+async def basic_welcome_to_everyone():
+    return {"Message": "Welcome to McDonald's Order system."}
+
+
+@app.post("/order")
+async def create_order(order: Order):
+    with Session(engine) as session:
+        session.add(order)
+        session.commit()
+        session.refresh(order)
+    return {
+        "Message": "Order added successfully",
+        "Order_id": f"#{order.id:03d}",
+        "order": order
+    }
+
+
+@app.get("/orders")
+async def kitchen_display():
+    return order_list
+
+
+@app.delete("/order/{order_id}")
+async def delete_Orders(order_id: str):
+    for order in order_list:
+        if order.id == order_id:
+            order_list.remove(order)
+            return {"Message": "Item deleted successfully"}
+    raise HTTPException(status_code=404, detail="Order not found")
+
+
+@app.put("/order/{order_id}/status")
+async def status(order_id: str, new_status: str):
+    for order in order_list:
+        if order.id == order_id:
+            order.status = new_status
+            return {"Message": "Item completed"}
+    raise HTTPException(status_code=404, detail="Order not found")
+
+
+@app.get("/order/completed")
+async def order_completed():
+    completed_orders = []
+    for order in order_list:
+        if order.status == "Completed":
+            completed_orders.append(order)
+    return completed_orders
+
+
+@app.get("/order/pending")
+async def order_pending():
+    pending = []
+    for order in order_list:
+        if order.status == "Pending":
+            pending.append(order)
+    return pending
+
+
+@app.get("/order/{order_id}")
+async def one_order(order_id: str):
+    for order in order_list:
+        if order.id == order_id:
+            return order
+    raise HTTPException(status_code=404, detail="Order not found.")
+
+
+@app.get("/order/total")
+async def order_total():
+    total = 0
+    for order in order_list:
+        total = total + order.price
+    return {"total_price": total, "order_count": len(order_list)}
+
+
+@app.get("/Welcome")
+async def welcome_back():
+    return {"Message": "Thank you for visiting, and welcome back."}
