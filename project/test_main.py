ccfe514c0fb6e8717bdeb6162a6e2c07152ec519
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-03-14 14:23:48 +0200

    I think about firstly I make every test in one file, because so much easy for start, and then when my project come bigger, so I will check do I need make another file or no

--- project/test_main.py
+++ project/test_main.py
@@ -1,8 +1,15 @@
+import pytest
 from fastapi.testclient import TestClient
-
 from main import app
 
 client = TestClient(app)
+
+
+def test_welcome():
+    response = client.get("/MCDONALDS")
+    assert response.status_code == 200
+    assert response.json() == {
+        "Message": "Welcome to McDonald's Order system."}
 
 
 def test_read_main():
