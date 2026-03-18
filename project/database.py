54f04c2d2ea283d05764edd0784fe536e2487979
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-03-18 14:34:18 +0200

    docker updates

--- project/database.py
+++ project/database.py
@@ -4,5 +4,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-engine = create_engine(os.getenv("database_url"))
+database_url = os.getenv("database_url")
+
+if not database_url:
+    raise ValueError("check your .env file")
+engine = create_engine(database_url)
 SQLModel.metadata.create_all(engine)
